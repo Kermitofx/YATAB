050448583d5c03096809c4a0b9929dc409269fe6
Kermitofx <39018448+Kermitofx@users.noreply.github.com>
2019-09-07 02:24:07 -0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from modules.commands.s0ng5 import s0ng
 from modules.commands.pong import pong
 from modules.commands.op import op,mode
 from modules.commands.ban import ban,unban
-from modules.commands.help import help
+from modules.commands.help import ajuda
 
 from modules.onjoin.notasciiname import no_ascii_name
 from modules.onjoin.nobots import nobots
